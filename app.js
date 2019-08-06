e461435f6e9bcba613c377e6aa803887d26e2cae
Lord of Destiny <djumicbranisav@outlook.com>
2019-08-06 13:21:34 +0200

    Initial commit

--- app.js
+++ app.js
@@ -23,21 +23,6 @@ app.get("/", (req, res) => {
   res.render("index", { greet: "Hello World!", title: "Charts" });
 });
 
-app.get("/test", (req, res) => {
-  const params = {
-    symbol: "BTC,ETH,XRP"
-  };
-  axios
-    .get(`${env.apiTarget()}/v1/cryptocurrency/map`, {
-      headers: { "X-CMC_PRO_API_KEY": env.apiKey() },
-      params
-    })
-    .then(response => {
-      res.render("test", { data: JSON.stringify(response.data) });
-    })
-    .catch(error => {
-      res.render("error", { msg: error.message });
-    });
-});
+app.get("/test", (req, res) => {});
 
 module.exports = app;
